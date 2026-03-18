ea21d23019f1c63f96cff1e357cdf614de0ab197
Destiny Duke <destiny.duke247@gmail.com>
2026-03-17 22:59:38 -0500

    Create 05_analysis_by_age.sql

---
+++ queries/05_analysis_by_age.sql
@@ -0,0 +1,38 @@
+WITH per_device AS ( 
+  SELECT 
+    impact.device_id, 
+    device.device_type, 
+    AVG(impact.energy_savings_yr) AS 
+avg_energy_savings_per_device, 
+    SUM(impact.co2_saved_kg_yr) AS 
+total_co2_saved_per_device, 
+    2024 - device.model_year AS device_age_2024 
+  FROM 
+    intel.impact_data AS impact 
+    JOIN intel.device_data AS device ON 
+device.device_id = impact.device_id 
+  GROUP BY 
+    impact.device_id, 
+    device.device_type, 
+    device.model_year 
+) 
+SELECT 
+  CASE  
+        WHEN (device_age_2024) <= 3 THEN 'Newer' 
+        WHEN (device_age_2024) > 3 AND 
+(device_age_2024) < 6 THEN 'Mid-Age' 
+        WHEN (device_age_2024) > 6 THEN 'Older' 
+    END AS device_age_bucket, 
+ 
+ 
+  COUNT(*) AS total_devices_repurposed, 
+  AVG(avg_energy_savings_per_device) AS 
+avg_energy_savings_kwh_per_device, 
+  SUM(total_co2_saved_per_device) / 1000.0 AS 
+total_co2_saved_tons 
+FROM 
+  per_device 
+GROUP BY 
+  device_age_bucket 
+ORDER BY 
+  device_age_bucket
